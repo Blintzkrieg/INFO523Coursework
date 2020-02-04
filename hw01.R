29f0631ff7b269329e2cee7ef56cf7a0dc30a79e
Angela Hughes <angela.hughes@email.arizona.edu>
2020-02-04 11:36:09 -0700

    adding the beginnings of hw01 R and docx files

---
+++ hw01.R
@@ -0,0 +1,7 @@
+ages <- c(13, 15, 16, 16, 19, 20, 20, 21, 22, 22, 25, 25, 25, 25, 30,33, 33, 35, 35, 35, 35, 36, 40, 45, 46, 52, 70)
+mean(ages)
+
+library(dplyr)
+installed.packages("modes")
+library(modes)
+modes(ages)
