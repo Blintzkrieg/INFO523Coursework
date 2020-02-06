6cb7a52d7a5617f7d3f8e0f26e4a619cb8108c50
Angela Hughes <angelahughes@email.arizona.edu>
2020-02-05 17:58:08 -0700

    still stuck on 2.8, but added more detail to comments in 2.3

--- hw01.R
+++ hw01.R
@@ -2,17 +2,16 @@
 #Page 80, Exercises 2.2, 2.3, 2.4, 2.8
 
 library(dplyr)
-library(ggplot2)
 
 #2.2(a) mean is 29.96296
-ages <- c(13, 15, 16, 16, 19, 20, 20, 21, 22, 22, 25, 25, 25, 25, 30,33, 33, 35, 35, 35, 35, 36, 40, 45, 46, 52, 70)
+ages <- c(13, 15, 16, 16, 19, 20, 20, 21, 22, 22, 25, 25, 25, 25, 30,
+          33, 33, 35, 35, 35, 35, 36, 40, 45, 46, 52, 70)
 mean(ages)
 
 #2.2(b) mode is 25. It occurs 4 times.
 install.packages("modes")
 library(modes)
 modes(ages)
-
 
 #2.2(c) midrange is 41.5
 max <- max(ages)
@@ -39,15 +38,21 @@ boxplot(ages)
 quantile(ages)
 qqnorm(ages)
 
-#2.3  need clarification
-#using formula (2.3) on page 46 of the textbook
+#2.3  Approximate median value.
+#using formula (2.3) on page 46 of the textbook.
+#where 21 is the lower boundary of the median interval, 
+#3194 is number of values in the entire dataset, 
+#950 is the sum of the frequencies of all the intervals that are lower than the median interval (200+450+300), 
+#and 29 is the median's bin width.
+#the midpoint is 55.5, which would fall in bin "51-80", but we know the median is going to be in the bin with highest frequency, "21-50".
 approxMed <- 21 + (((3194/2) - 950)/1500)*29
-
+print(approxMed)
 
 
 #2.4(a) mean, median, and standard deviation for ages and %fat
 age <- c(23,23,27,27,39,41,47,49,50,52,54,54,56,57,58,58,60,61)
-percentfat <- c(9.5,26.5,7.8,17.8,31.4,25.9,27.4,27.2,31.2,34.6,42.5,28.8,33.4,30.2,34.1,32.9,41.2,35.7)
+percentfat <- c(9.5,26.5,7.8,17.8,31.4,25.9,27.4,27.2,31.2,34.6,
+                42.5,28.8,33.4,30.2,34.1,32.9,41.2,35.7)
 
 mean(age)
 sd(age)
@@ -70,30 +75,32 @@ qqplot(age, percentfat)
 install.packages("philentropy")
 library(philentropy)
 
-#this is the two-dimensional dataset of objects and properties on page 81
-xValues <- c(1,2,3,4,5)
-#A1 and A2 plus the new data point x
-a1 <- c(1.5,2,1.6,1.2,1.5, 1.4)
-a2 <- c(1.7,1.9,1.8,1.5,1.0, 1.6)
+#the 2-D data set before adding the new data point
+dataSetA = cbind(c(1.5,2,1.6,1.2,1.5), c(1.7,1.9,1.8,1.5,1.0))
+rownames(dataSetA) <- c('x1', 'x2', 'x3', 'x4', 'x5')
+colnames(dataSetA) <- c('A1', 'A2')
+print(dataSetA)
+plot(dataSetA)
 
-#twoDdataSet = cbind(c(1.5,2,1.6,1.2,1.5), c(1.7,1.9,1.8,1.5,1.0))
-
-twoDdataSetb = cbind(c(1.5,2,1.6,1.2,1.5, 1.4), c(1.7,1.9,1.8,1.5,1.0, 1.6))
-rownames(twoDdataSetb) <- c('x1', 'x2', 'x3', 'x4', 'x5', 'x')
-colnames(twoDdataSetb) <- c('A1', 'A2')
+#the 2-D data set after adding the new data point x=(1.4, 1.6)
+dataSetB = cbind(c(1.5,2,1.6,1.2,1.5, 1.4), c(1.7,1.9,1.8,1.5,1.0, 1.6))
+rownames(dataSetB) <- c('x1', 'x2', 'x3', 'x4', 'x5', 'x')
+colnames(dataSetB) <- c('A1', 'A2')
+print(dataSetB)
+plot(dataSetB)
 
 #newDataPoint = rbind(c(1.4,1.6))
 
 #Euclidean distance
-dist(twoDdataSetb, method="euclidean")
+dist(dataSetB, method="euclidean")
 #Database points in order of similarity with x (most to least similar): 
 #x2, x5, x3, x4, x1
 
 #Manhattan distance
-dist(twoDdataSetb, method="manhattan")
+dist(dataSetB, method="manhattan")
 
 #supremum distance
-dist(twoDdataSetb, method="maximum")
+dist(dataSetB, method="maximum")
 
 
 #cosine similarity
@@ -101,7 +108,10 @@ dist(twoDdataSetb, method="maximum")
 #cosine_dist(a1, a2, testNA=FALSE)
 
 #2.8(b)
-#normalized and ranked using Euclidean distance
+#normalized
+
+
+#ranked using Euclidean distance
 
 
 
