ac14d5d31553721f0c4c104d52ad2dcbbff7d7de
hannahre <hannahreandrews@gmail.com>
2020-02-22 15:08:52 -0700

    Files I was referring to were not in our folder!
    Okay now you can see the merge files and the new (hopefully improved) MIDUS2 data.

---
+++ know-your-data.R
@@ -0,0 +1,11 @@
+# Know Your Data 
+# Hannah Andrews & Angela Hughes 
+# INFO 523 
+# Spring 2020 
+
+load(file = "C:/Users/hanna/Documents/git/INFO523 Coursework/MIDUS2.rda")
+
+head(MIDUS2)
+
+# Central tendency measures
+mean(as.numeric(MIDUS2$B1PDEPRE))
